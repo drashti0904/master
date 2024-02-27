09fdce90a03a27f46d0a61b65f91f0527b7f376b
Drashti Desai <83410796+drashti0904@users.noreply.github.com>
2024-02-27 14:43:19 +0000

    done1

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
+
 const express = require("express");
 const mongoose = require("mongoose");
+const mongoData = process.env.DATABASE_URL;
+mongoose.connect(mongoData)
+const database = mongoose.connection;
 
+database.on('error', (error) =>{
+  console.log(error)
+})
+database.once('connected',()=>{
+  console.log('database connected')
+})
 const app = express();
 app.use(express.json());
+const routes = require('./routes/routes')
 
+app.use('/api',routes)
 app.listen(3000, () => {
   console.log(`server started at${3000}`);
 });
